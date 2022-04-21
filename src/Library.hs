363ab67a2a4e36a187f816875dea27c11470e3bd
pepinony <102746551+pepinony@users.noreply.github.com>
2022-04-21 15:45:14 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -12,15 +12,22 @@ data Hechicero = Hechicero {
 
 nobara = Hechicero 1 "Kugisaki" 3
 
+esEspecial :: Hechicero->Bool
+esEspecial hehcicero = grado hechicero == 0
+
 tieneExperiencia :: Hechicero -> Bool
 tieneExperiencia hechicero = antiguedad hechicero > 1
 
-equipoEstaPreparado :: [Hechicero]->Bool
+type Equipo = [Hechicero]
+
+equipoEstaPreparado :: Equipo->Bool
 equipoEstaPreparado equipo = length equipo > 3
  
 subirDeGrado :: Hechicero -> Hechicero
-subirDeGrado hechicero | grado hechicero == 0 = hechicero
-                       | otherwise = Hechicero (antiguedad hechicero) (clan hechicero)  (grado hechicero - 1)
+subirDeGrado hechicero | esEspecial hechicero = hechicero
+                       | otherwise = hechicero {grado hechicero = (grado hechicero - 1)}
+                       
+clanesPrestigioso = ["Gojo", "Kamo", "Zenine"]
 
 esPrestigioso:: Hechicero->Bool
-esPrestigioso hechicero = clan hechicero == "Gojo" || clan hechicero == "Kamo" ||clan hechicero == "Zenine"
+esPrestigioso hechicero = elem clan hechicero clanesPrestigiosos

7e4b96ad75cf9741f9d7bf8e71e5294edebed24f
pepinony <102746551+pepinony@users.noreply.github.com>
2022-04-21 10:41:51 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,24 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+data Hechicero = Hechicero {
+    antiguedad :: Number,
+    clan :: String,
+    grado :: Number
+} deriving (Eq, Show)
+
+nobara = Hechicero 1 "Kugisaki" 3
+
+tieneExperiencia :: Hechicero -> Bool
+tieneExperiencia hechicero = antiguedad hechicero > 1
+
+equipoEstaPreparado :: [Hechicero]->Bool
+equipoEstaPreparado equipo = length equipo > 3
+ 
+subirDeGrado :: Hechicero -> Hechicero
+subirDeGrado hechicero | grado hechicero == 0 = hechicero
+                       | otherwise = Hechicero (antiguedad hechicero) (clan hechicero)  (grado hechicero - 1)
+
+esPrestigioso:: Hechicero->Bool
+esPrestigioso hechicero = clan hechicero == "Gojo" || clan hechicero == "Kamo" ||clan hechicero == "Zenine"
